4620cebad01adc109641a7aaa2f578a4228cd49a
Nishant Tomer <nishanttomer5601@gmail.com>
2024-02-27 19:21:37 +0530

    Added controllers for likes and health

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,68 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+  // built a healthcheck response that simply returns the OK status as json with a message
+  const channelStats = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "_id",
+        foreignField: "owner",
+        as: "videos",
+        pipeline: [
+          {
+            $lookup: {
+              from: "likes",
+              localField: "_id",
+              foreignField: "likedBy",
+              as: "likes",
+              pipeline: [
+                {
+                  $addFields: {
+                    Videolikes: {
+                      $size: "$likes",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        totalViews: {
+          $sum: "$videos.views",
+        },
+        totalLikes: {
+          $sum: "$videos.VideoLikes",
+        },
+        totalVideos: {
+          $size: "$videos",
+        },
+      },
+    },
+  ]);
 
-export {
-    healthcheck
-    }
-    
+  if (!channelStats) throw new ApiError(500, "No data available");
+
+  res.status(200).json(new ApiResponse(200, channelStats, "Success"));
+});
+
+export { healthcheck };
